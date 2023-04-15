dc8f5f3fbda65260dc7df45fda3be84ebe4581d2
Exxen <98343957+exxxen@users.noreply.github.com>
2023-04-15 13:00:04 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,13 @@
+local id = game.PlaceId
 
+local supported = syn.request({
+    Url = "https://raw.githubusercontent.com/exxxen/supernova/main/supported.json",
+    Method = "GET"
+})["Body"]
+supported = game:GetService("HttpService"):JSONDecode(supported)
+
+for i,v in pairs(supported) do
+    if id == v then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/exxxen/supernova/main/scripts/"..i..".lua"))()
+    end
+end
